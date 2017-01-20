7a5cc95f4aa96fe6e429caa40c61ea6a1c58faf4
daanhenke <daanhenkers@gmail.com>
2017-01-20 08:23:58 +0100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -8,10 +8,10 @@ function engine_ready() {
         map = new Map(20, 16, 12, 0, 32, 16, ["gray", "blue"]);
 
         map.generate_heightmap(0);
-        //map.set_cube(4, 8, 0, 4, 5, 9, 1);
+        map.set_cube(4, 8, 0, 4, 5, 9, 1);
 
-        GameEngine.RenderManager.drawMap(300, 100, map);
-        //GameEngine.RenderManager.drawMapWithDelayEffectLolY(300, 100, map, 1);
+        //GameEngine.RenderManager.drawMap(300, 100, map);
+        GameEngine.RenderManager.drawMapWithDelayEffectLolY(300, 100, map, 1);
     }});
 }
 
